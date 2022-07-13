a652cd39b9c4b8021a117f1523f9efe90b4acaeb
RSaucedo96 <ricardoarielsaucedo96@gmail.com>
2022-07-13 15:03:28 -0300

    finished play round function

--- rps.js
+++ rps.js
@@ -11,8 +11,25 @@ function computerPlay(){
     }
 }
 
+function playRound(computerSelection , playerSelection){
+    if (computerPlay === playerSelection){
+        return("the computer says:" + computerSelection + " ,it's a Draw!!");
+    }
+    else if (computerSelection === "rock" && playerSelection === "paper"){
+        return("the computer says:" + computerSelection + " ,You won!!");
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissors"){
+        return("the computer says:" + computerSelection + " ,You won!!");
+    }
+    else if (computerSelection === "scissors" && playerSelection === "rock"){
+        return("the computer says:" + computerSelection + " ,You won!!");
+    }
+    else {
+        return ("the computer says: " + computerSelection + " ,You lost.");
+    }
+}
 
 let playerChoice = prompt("Rock, Paper, Scissors!:");
 let playerSelection = playerChoice.toLowerCase();
 const computerSelection = computerPlay();
-console.log(computerSelection);
+console.log(playRound(computerSelection, playerSelection));
